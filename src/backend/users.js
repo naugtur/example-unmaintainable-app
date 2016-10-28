2b95f9a593b9daab2517509502a2322b2a5b722f
Paweł Pisarek <pisarekp73@gmail.com>
2016-10-28 22:59:35 +0200

    small fix

--- src/backend/users.js
+++ src/backend/users.js
@@ -1,16 +1,15 @@
 const router = require('express').Router();
-const errorHandler = require('./errorHandler');
 
 router.get('/users', (req, res) => {
     req.app.get('UsersService').callbackMeTheListing((result, err) => {
-        errorHandler.returnResponse(result, err, res);
+        req.app.get('ErrorHandler').returnResponse(result, err, res);
     });
 });
 
 router.get('/users/:userId', (req, res) => {
     const userId = parseInt(req.params.userId);
     req.app.get('UsersService').callbackMeTheOne(userId, (result, err) => {
-        errorHandler.returnResponse(result, err, res);
+        req.app.get('ErrorHandler').returnResponse(result, err, res);
     });
 
 });
