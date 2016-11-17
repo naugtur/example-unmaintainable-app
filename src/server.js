c1189039993f6f8ab0e9d162d8f6007653489834
Wojciech Olejnik <wojciech.olejnik@netguru.co>
2016-11-17 19:34:11 +0100

    Basic refactoring

--- src/server.js
+++ src/server.js
@@ -1,20 +1,13 @@
 const express = require('express');
-const usersRoutes = require('./backend/users/routes');
+const routes = require('./backend/routes');
 
 let app = express();
-
-app = userRoutes(app);
-
-app.get('/api/users', handlers.list)
-    app.get('/api/users/:userId', handlers.getOne)
-
-        app.use('/api/users', bodyParser.json())
-    app.post('/api/users', handlers.addOne)
+app = routes(app);
 
 app.use('/front', express.static(__dirname + '/front'));
 
 module.exports = {
-    start(){
-        app.listen(1337)
-    }
+  start() {
+    app.listen(1337);
+  }
 }
