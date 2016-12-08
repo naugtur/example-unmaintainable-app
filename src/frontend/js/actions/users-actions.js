fa8568c463366f298d9775b779bde34f7f90e770
Wojciech Olejnik <wojciech.olejnik@netguru.co>
2016-12-09 00:01:06 +0100

    Small eslint corrections

--- src/frontend/js/actions/users-actions.js
+++ src/frontend/js/actions/users-actions.js
@@ -1,4 +1,3 @@
-import Immutable from 'immutable';
 import {
   APIcall,
   USERS_ENDPOINT,
--- src/frontend/js/reducers/users-reducer.js
+++ src/frontend/js/reducers/users-reducer.js
@@ -9,20 +9,20 @@ export const CONSTRUCT = () => Immutable.fromJS({
     twitter: '',
     memberFor: '',
   }
-})
+});
 
-export const FETCH_USERS_REQUEST = (state, action) => state;
+export const FETCH_USERS_REQUEST = state => state;
 
 export const FETCH_USERS_SUCCESS = (state, action) => (
   state.set('users', Immutable.fromJS(action.payload.users))
 );
 
-export const FETCH_USERS_FAILURE = (state, action) => state;
+export const FETCH_USERS_FAILURE = state => state;
 
-export const FETCH_USER_REQUEST = (state, action) => state;
+export const FETCH_USER_REQUEST = state => state;
 
 export const FETCH_USER_SUCCESS = (state, action) => (
   state.set('currentUser', Immutable.fromJS(action.payload))
 );
 
-export const FETCH_USER_FAILURE = (state, action) => state;
+export const FETCH_USER_FAILURE = state => state;
